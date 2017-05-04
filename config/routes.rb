5eb67cfd6dbfd4fcbb173b5f132f3743a8a38897
Jerel Lim <j.limster@gmail.com>
2017-05-04 17:33:33 +0800

    added orders files

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
+
   root 'supplier#index'
 
   resources:supplier
+
+  resources:orders
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
