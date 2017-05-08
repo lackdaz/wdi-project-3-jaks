3a16803667ad4cd4c3b597e4c998d3c7b4a59df4
seth loh <haloandwings@gmail.com>
2017-05-08 11:41:36 +0800

    before code breaking

--- app/assets/javascripts/transactions.js
+++ app/assets/javascripts/transactions.js
@@ -1,9 +1,9 @@
-$(document).on('ready page:load', function () {
-  if($('#all-transactions').length) {
-    alert('ello ello ello')
-    // gon.flights.forEach(function(data){
-    //   console.log(JSON.stringify(data))
-    // })
-    
-  }
-})
+// $(document).on('ready page:load', function () {
+//   if($('#all-transactions').length) {
+//     alert('ello ello ello')
+//     // gon.flights.forEach(function(data){
+//     //   console.log(JSON.stringify(data))
+//     // })
+//
+//   }
+// })
