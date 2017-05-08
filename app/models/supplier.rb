f33db448d7216fe61ae43c793cc4a4b401cf0b0e
Jerel Lim <j.limster@gmail.com>
2017-05-08 11:56:53 +0800

    merged and attempting search bar

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,2 +1,5 @@
 class Supplier < ApplicationRecord
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable ,:registerable, :confirmable
 end
