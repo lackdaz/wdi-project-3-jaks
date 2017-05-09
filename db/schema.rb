bd1ecca68b6793f37515cff5886e949f977d689c
Jerel Lim <j.limster@gmail.com>
2017-05-09 13:40:21 +0800

    before merge

--- db/schema.rb
+++ db/schema.rb
@@ -128,6 +128,10 @@ ActiveRecord::Schema.define(version: 20170509025252) do
     t.datetime "locked_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
