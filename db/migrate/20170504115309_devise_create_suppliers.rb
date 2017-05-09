52adf7ea50f178d1fb7f258f5319bc0f75684df6
masloord <masloord@gmail.com>
2017-05-09 15:23:25 +0800

    added supplier custom field

--- db/migrate/20170504115309_devise_create_suppliers.rb
+++ db/migrate/20170504115309_devise_create_suppliers.rb
@@ -7,6 +7,10 @@ class DeviseCreateSuppliers < ActiveRecord::Migration[5.0]
       t.string :encrypted_password, null: false, default: ""
 
       #custom fields
+      t.string :address
+      t.string :name
+      t.integer :contact
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
--- db/schema.rb
+++ db/schema.rb
@@ -120,6 +120,8 @@ ActiveRecord::Schema.define(version: 20170509064314) do
     t.string   "name"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.string   "address"
+    t.integer  "contact"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -148,15 +150,14 @@ ActiveRecord::Schema.define(version: 20170509064314) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
     t.integer  "failed_attempts",        default: 0,  null: false
     t.string   "unlock_token"
     t.datetime "locked_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
