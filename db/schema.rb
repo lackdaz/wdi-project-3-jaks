9860aa654750c3ec2c9a81a05f798897c0c1218d
seth loh <haloandwings@gmail.com>
2017-05-09 15:25:19 +0800

    added kenneths new features

--- db/schema.rb
+++ db/schema.rb
@@ -120,8 +120,6 @@ ActiveRecord::Schema.define(version: 20170509064314) do
     t.string   "name"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
-    t.string   "address"
-    t.integer  "contact"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
