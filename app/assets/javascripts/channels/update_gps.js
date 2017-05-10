49716f979d2341a894505c9d004c78cf9b0378fd
seth loh <haloandwings@gmail.com>
2017-05-10 22:39:12 +0800

    pulled from master

--- app/assets/javascripts/channels/update_gps.js
+++ app/assets/javascripts/channels/update_gps.js
@@ -8,30 +8,11 @@ App.update_gps = App.cable.subscriptions.create('UpdateGpsChannel', {
     // return alert('disconnected')
   },
   received: function (data) {
-    // var $target = $('#flight-list')
-
-    // var dummy = {
-    //   lat: 1.3200,
-    //   lng: 103.8439
-    // }
 
     var gps = {
       lat: parseFloat(data.content.split(',').shift()),
       lng: parseFloat(data.content.split(',').pop())
     }
-
-    // var newLatLng = new google.maps.LatLng({
-    //   lat: gps.lat,
-    //   lng: gps.lng
-    // })
-
-    // var map = new google.maps.Map(document.getElementById('map'), {
-    //   center: {
-    //     lat: gps.lat,
-    //     lng: gps.lng
-    //   },
-    //   zoom: 13
-    // })
 
     deleteMarkers(markersArray)
 
@@ -54,6 +35,4 @@ App.update_gps = App.cable.subscriptions.create('UpdateGpsChannel', {
       markersArray = []
     }
   }
-})
-//   }
-// })
+})  
