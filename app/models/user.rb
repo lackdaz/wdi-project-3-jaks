3f99cfcde16f914c3da5a138a41f5f44342a0462
zhenhao goh <kenneth.gzhao@outlook.com>
2017-05-10 19:43:45 +0800

    try again

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable ,:registerable
+         :recoverable, :rememberable, :trackable, :validatable ,:registerable, :confirmable
+
   has_many :delivery_addresses
 end
